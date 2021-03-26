d2b5fcfd632d199c81882b3cb674f72a90b311ab
patrickjolliffe <jolliffe@gmail.com>
2021-03-26 12:10:18 +0000

    Update testcase.sql

--- testcase.sql
+++ testcase.sql
@@ -3,7 +3,7 @@ alter session set "_small_table_threshold"=1;
 drop table fcwrnc purge;
 create table fcwrnc (b blob, nc nchar(30)) storage(cell_flash_cache keep);
 alter session set cell_offload_processing=FALSE;
-insert /*+append*/ into fcwrnc (b, nc) select utl_raw.cast_to_raw('R'), 'XXX' || case when mod(rownum, 1000) = 0 then '7' else '0' end || '99_' from dual connect by level < 52604  ;
+insert /*+append*/ into fcwrnc (b, nc) select null, 'XXX' || case when mod(rownum, 1000) = 0 then '7' else '0' end || '99_' from dual connect by level < 52604  ;
 COMMIT;
 select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'XXX7%' group by to_char(sysdate, 'hh24:mi:ss');
 
@@ -11,3 +11,4 @@ alter session set cell_offload_processing=TRUE;
 select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'XXX7%' group by to_char(sysdate, 'hh24:mi:ss');
 /*Wait a while, between 1 and 10 minutes*/ 
 select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'XXX7%' group by to_char(sysdate, 'hh24:mi:ss');
+exit;
