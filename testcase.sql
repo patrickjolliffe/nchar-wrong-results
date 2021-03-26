1a853fd7701099802b867fede5c7207007694bcd
patrickjolliffe <jolliffe@gmail.com>
2021-03-26 12:12:20 +0000

    Update testcase.sql

--- testcase.sql
+++ testcase.sql
@@ -3,12 +3,12 @@ alter session set "_small_table_threshold"=1;
 drop table fcwrnc purge;
 create table fcwrnc (b blob, nc nchar(30)) storage(cell_flash_cache keep);
 alter session set cell_offload_processing=FALSE;
-insert /*+append*/ into fcwrnc (b, nc) select null, 'XXX' || case when mod(rownum, 1000) = 0 then '7' else '0' end || '99_' from dual connect by level < 52604  ;
+insert /*+append*/ into fcwrnc (b, nc) select null, 'X' || case when mod(rownum, 1000) = 0 then '7' else '0' end || '99_' from dual connect by level < 52604  ;
 COMMIT;
-select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'XXX7%' group by to_char(sysdate, 'hh24:mi:ss');
+select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'X7%' group by to_char(sysdate, 'hh24:mi:ss');
 
 alter session set cell_offload_processing=TRUE;
-select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'XXX7%' group by to_char(sysdate, 'hh24:mi:ss');
+select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'X7%' group by to_char(sysdate, 'hh24:mi:ss');
 /*Wait a while, between 1 and 10 minutes*/ 
-select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'XXX7%' group by to_char(sysdate, 'hh24:mi:ss');
+select to_char(sysdate, 'hh24:mi:ss'), count(*) from fcwrnc where nc like 'X7%' group by to_char(sysdate, 'hh24:mi:ss');
 exit;
