87c969538e267978bae7c1b3167831a3691d2094
patrickjolliffe <jolliffe@gmail.com>
2021-03-27 11:36:37 +0000

    Update testcase.sql

--- testcase.sql
+++ testcase.sql
@@ -1,12 +1,12 @@
 alter session set "_small_table_threshold"=1;
 drop table fcwrnc purge;
-create table fcwrnc (nc nchar(20)) storage(cell_flash_cache keep);
+create table fcwrnc (nc nchar(8)) storage(cell_flash_cache keep);
 alter session set cell_offload_processing=FALSE;
-insert /*+append*/ into fcwrnc (nc) select case when mod(rownum, 1000) = 0 then '7' else '0' end || '9_' from dual connect by level < 52604  ;
+insert /*+append*/ into fcwrnc (nc) select case when mod(rownum, 1000) = 0 then '1' else '0' end from dual connect by level < 52604  ;
 COMMIT;
-select count(*) from fcwrnc where nc like '7%';
+select count(*) from fcwrnc where nc like '1%';
 alter session set cell_offload_processing=TRUE;
-select count(*) from fcwrnc where nc like '7%';
+select count(*) from fcwrnc where nc like '1%';
 exec sys.dbms_session.sleep(1);
-select count(*) from fcwrnc where nc like '7%';
+select count(*) from fcwrnc where nc like '1%';
 exit;
