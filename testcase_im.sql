81a65f8130c4d27837c4f9f3d4f29aca0b825633
patrickjolliffe <jolliffe@gmail.com>
2021-03-31 11:54:44 +0100

    Update testcase_im.sql

--- testcase_im.sql
+++ testcase_im.sql
@@ -1,6 +1,6 @@
 drop table fcwrnc purge;
 create table fcwrnc (nc nvarchar2(1)) inmemory;
-insert /*+append*/ into fcwrnc (nc) select mod(rownum, 10) from dual connect by level < 4000;
+insert /*+append*/ into fcwrnc (nc) select mod(rownum, 2) from dual connect by level < 4000;
 commit;
 select count(*) from fcwrnc where nc like '1%';
 exec sys.dbms_session.sleep(1);
