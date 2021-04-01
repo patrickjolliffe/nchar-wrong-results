48b4a9b9e8493defb1928e8562449b1544382520
patrickjolliffe <jolliffe@gmail.com>
2021-04-01 09:33:27 +0100

    Update testcase_im.sql

--- testcase_im.sql
+++ testcase_im.sql
@@ -1,8 +1,21 @@
 drop table fcwrnc purge;
+
 create table fcwrnc (nc nvarchar2(1)) inmemory;
+
 insert /*+append*/ into fcwrnc (nc) select mod(rownum, 2) from dual connect by level < 4000;
+
 commit;
+
 select count(*) from fcwrnc where nc like '1%';
+
 exec sys.dbms_session.sleep(1);
+
 select count(*) from fcwrnc where nc like '1%';
+
+alter session set "_kdz_pcode_flags" = 1;
+
+select count(*) from fcwrnc where nc like '1%';
+
 exit;
+
+
