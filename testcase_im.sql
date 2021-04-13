ae0266d56a68bef06b59ec80f3287cfba2c308c8
patrickjolliffe <jolliffe@gmail.com>
2021-04-13 10:32:16 +0100

    Update testcase_im.sql

--- testcase_im.sql
+++ testcase_im.sql
@@ -12,6 +12,8 @@ exec sys.dbms_session.sleep(1);
 
 select count(*) from fcwrnc where nc like '1%';
 
+select /*_no_inmemory */ count(*) from fcwrnc where nc like '1%';
+
 alter session set "_kdz_pcode_flags" = 1;
 
 select count(*) from fcwrnc where nc like '1%';
